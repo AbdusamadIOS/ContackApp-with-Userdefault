a22ee6601fa1939f2764681d6e38935554765720
abdusamad <abdusamad2609@gmail.com>
2023-05-12 17:04:50 +0500

    Initial Commit

---
+++ ContackApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ContackApp
+//
+//  Created by Abdusamad Mamasoliyev on 12/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
